ed0bf31fc63db632e613def13721485d0a7c1b80
DAL Laptop <david.a.leber@gmail.com>
2021-06-29 21:56:57 -0700

    Adds initial class outlines

---
+++ Document.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 21:51:27 2021
+
+@author: david
+"""
+
+class Document:
+    
+    
---
+++ Page.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 21:52:08 2021
+
+@author: david
+"""
+
+class Page:
+    
