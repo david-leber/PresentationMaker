69c94f207f25ab2772628479627f97a8d8d21064
DAL Laptop <david.a.leber@gmail.com>
2021-06-30 23:04:27 -0700

    Minor Lint improvements

--- Code/Field.py
+++ Code/Field.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 30 21:11:46 2021
+Created on Wed Jun 30 21:11:46 2021.
 
 @author: david
 """
 
+
 class Field:
-    
+    """Represents a single content field on a Page."""
+
     def __init__():
         pass
-    
